f9727fed5279c00a67a2ecd404b94ef2e9fedfcb
HYUNJUN KIM <hyunjun1121@kaist.ac.kr>
2024-05-23 16:14:56 +0900

    Title: 4번의 크기 비교, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240523/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,23 @@
+a =int(input())
+arr = list(map(int,input().split()))
+b=arr[0]
+c=arr[1]
+d=arr[2]
+e=arr[3]
+
+if a>b:
+    print(1)
+else:
+    print(0)
+if a>c:
+    print(1)
+else:
+    print(0)
+if a>d:
+    print(1)
+else:
+    print(0)
+if a>e:
+    print(1)
+else:
+    print(0)
