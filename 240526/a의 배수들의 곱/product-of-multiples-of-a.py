253bbd5ad6b95de81b8cbaeab5faf9b5823a6859
HYUNJUN KIM <hyunjun1121@kaist.ac.kr>
2024-05-26 15:08:35 +0900

    Title: a의 배수들의 곱, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240526/a의 배수들의 곱/product-of-multiples-of-a.py
@@ -0,0 +1,10 @@
+arr = list(map(int,input().split()))
+a = arr[0]
+b = arr[1]
+new_list=[]
+for i in range(b):
+    if (i+1)%a ==0:
+        new_list.append(i+1)
+number = 1
+for j in new_list:
+    number *= j
