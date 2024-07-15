7431f97da9d03677634dbd02158f92d4e4181d8b
HYUNJUN KIM <hyunjun1121@kaist.ac.kr>
2024-07-15 15:45:52 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 142ms, Memory: 26MB, Status: Passed - CodeTree

--- 240715/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
+++ 240715/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
@@ -1,7 +1,11 @@
 n = int(input())
 a=0
 for i in range(n):
-    print('*'*(n-i))
+    for j in range((n-i)):
+        print('*', end= ' ')
+    print()
 
 for i in range(n):
-    print('*'*(i+1))
+    for j in range((i+1)):
+        print('*', end = ' ')
+    print()
