d636b65dad078f0b01a3a6410f0085a45bebd143
HYUNJUN KIM <hyunjun1121@kaist.ac.kr>
2024-07-26 13:17:19 +0900

    Title: 비오는 날, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240726/비오는 날/rainy-day.py
+++ 240726/비오는 날/rainy-day.py
@@ -1,7 +1,9 @@
 n = int(input())
-
+date_strings = []
 for i in range(n):
     a = input().split()
     if a[2] == 'Rain':
-        print(a[0]+' '+ a[1]+' '+a[2])
-        break
+        date_strings.append(str(a[0]+' '+ a[1]+' '+a[2]))
+sorted_date_strings = sorted(date_strings, key=lambda date: (date[:4], date[5:7], date[8:]))
+
+print(sorted_date_strings[0])
