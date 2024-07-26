be8ab85671fe84df33d059358c6aeaffe8ea8a51
HYUNJUN KIM <hyunjun1121@kaist.ac.kr>
2024-07-26 13:08:31 +0900

    Title: 중앙값 계산 2, Time: 110ms, Memory: 25MB, Status: Passed - CodeTree

--- 240726/중앙값 계산 2/get-median-2.py
+++ 240726/중앙값 계산 2/get-median-2.py
@@ -2,9 +2,13 @@ n = int(input())
 
 n_list = list(map(int, input().split()))
 
-def middle(a):
-    return n_list[int(a/2)]
+def middle(i): 
+    m_list= []
+    for j in range(i+1):
+        m_list.append(n_list[j])
+    m_list.sort()
     
+    return m_list[int(i/2)]    
 
 for i in range(n):
     if i%2==0: #홀수 일 때
